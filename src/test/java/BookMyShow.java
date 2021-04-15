dea53049a4a7d0f2e7cf9255dd4b72a8b9555753
Apzal Bahin <Apzal.Bahin@gds.ey.com>
2021-04-16 05:22:02 +0530

    added changes

--- src/test/java/BookMyShow.java
+++ src/test/java/BookMyShow.java
@@ -1,6 +1,5 @@
 import com.aventstack.extentreports.Status;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
